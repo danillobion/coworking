758b3e2fa4a2e18ac280d795759428f536af5654
Danillo Bion <danilobion@gmail.com>
2020-06-05 00:15:31 -0300

    Correcao - responsividade da tela news

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -6,62 +6,12 @@
       <div class="col-md-8 titulo2" style="margin-bottom:1rem;margin-top: -1rem;text-align:center;">News</div>
   </div>
 </div>
-@if(isset($destaqueNews->destaque) == 1)
-  <div style="background-color:#2abf9a; margin-bottom:2rem; padding-bottom:2rem;">
-    <div class="container">
-      <div class="row justify-content-center">
-          <div class="col-md-8">
-            <div class="row justify-content-center">
-              <div class="col-md-12">
-                <a class="btn" href="{{ route('show_news', ['idNews'=>$destaqueNews->id,]) }}" style="padding-left: 0rem; ">
-                  @if(strlen($destaqueNews->titulo) > 72)
-                    <?php $detaa = substr($item->titulo, 0, 72) ?>
-                    <div class="titulo_card" style="text-align:left; font-size:30px">{{$detaa}}...</div>
-                  @else
-                    <div class="titulo_card" style="text-align:left; font-size:30px">{{$destaqueNews->titulo}}</div>
-                  @endif
-                </a>
-              </div>
-              <div class="col-md-4">
-                @if(isset($destaqueNews->imagemCapa) && $destaqueNews->imagemCapa!="")
-                  <td><img src="{{asset('storage/imagens/news/' . $destaqueNews->imagemCapa)}}" alt="..." style="border-radius: 15px; width:220px"></td>
-                @endif
-              </div>
-              <div class="col-md-8">
-                <div class="col-md-12">
-                  <div class="row" style="margin-left:-15px; margin-right:-15px;">
-                    <div class="col-sm-3 subtitulo_card" style="color:black">{{$destaqueNews->created_at->format('d-m-Y')}}</div>
-                    <div class="col-sm-1 subtitulo_card" style="color:black">{{$destaqueNews->visualizacao}}</div>
-                  </div>
-                </div>
-                <div class="col-md-12">
-                  <div class="row" style="margin-left:-15px; margin-right:-15px;">
-                    @if(strlen($destaqueNews->conteudo) > 150)
-                      <?php
-                          $text1 = strip_tags($destaqueNews->conteudo);
-                          $deta = substr($text1, 0, 150);
-                      ?>
-                      <div class="col-md-12 detalhe_card">{!! $deta !!}...</div>
-                    @else
-                      <?php $deta = strip_tags($destaqueNews->conteudo)?>
-                      <div class="col-md-12 detalhe_card">{!! $deta !!}</div>
-                    @endif
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-      </div>
-    </div>
-  </div>
-@endif
-
 <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-8" style="margin-bottom:1rem;">
         @foreach ($allNews as $item)
         <ul class="list-group" style="margin-bottom:1rem;">
-          <li class="list-group-item" style="cursor:pointer;">
+          <li class="list-group-item styleCardNews" style="cursor:pointer;">
             <a class="btn" href="{{ route('show_news', ['idNews'=>$item->id,]) }}">
               <div class="row">
                 <div class="col-md-3" style="text-align:center">
